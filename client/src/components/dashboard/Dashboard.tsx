52a818a8978cd40a79d4a8635a58997c37bbb45d
beneenfune <bfune@ucsd.edu>
2024-11-06 14:54:18 -0800

    Dashboard date header updates automatically

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import TodaysTasks from "./TodaysTasks";
 import TodaysSleep from "./TodaysSleep";
-import HeartContainer from "./HeartsContainer";
 import WeeklyProgress from "./WeeklyProgress";
 import "./Dashboard.css";
 
 const Dashboard = () => {
+
+  // Get today's date and format it
+  const today = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <div className="dashboard-container">
-      <h1>November 4, 2024</h1>
+      <h1>{today}</h1> 
       <div className="dashboard-content">
         <div className="side-components">
           <TodaysTasks />
           <TodaysSleep />
         </div>
       </div>
-      <WeeklyProgress />{" "}
+      <WeeklyProgress />
     </div>
   );
 };
