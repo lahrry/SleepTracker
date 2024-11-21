4081ea8ce46af18b671438c889ec411adf452c9a
Ciro Zhang <ciz001@ucsd.edu>
2024-11-20 21:40:32 -0800

    big bug fix, moved all fetching to dashboard.txs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,19 @@
 import express from 'express';
-import cors from 'cors'; // Import cors
+import cors from 'cors';
+import taskRoutes from './tasks_backend/task-endpoints';
+import sleepRoutes from './sleep_backend/sleep-endpoints'; 
 
 const app = express();
 
-// Enable CORS
+// Enable CORS and JSON parsing
 app.use(cors());
-
 app.use(express.json());
 
-// Import and use your routes
-import taskRoutes from './task-endpoints';
-app.use('/api/v1', taskRoutes);
+// Register routes
+app.use('/api/v1/tasks', taskRoutes); 
+app.use('/api/v1/sleep', sleepRoutes); 
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
